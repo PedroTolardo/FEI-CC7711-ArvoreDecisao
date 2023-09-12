8ec64a810068035401082b63146f59cfad81058b
PedroTolardo <109559819+PedroTolardo@users.noreply.github.com>
2023-09-12 23:31:15 +0000

    alteração para a P1 e P2

--- CriterioMedia.py
+++ CriterioMedia.py
@@ -6,25 +6,28 @@ import matplotlib.pyplot as plt
 from scipy.io import arff
 
 def criterioMedia():
-    data,meta = arff.loadarff('./CriterioMedia.arff')
+    data,meta = arff.loadarff('./CriterioProvas.arff')
 
     attributes = meta.names()
     data_value = np.asarray(data)
 
 
     percFalta = np.asarray(data['PercFalta']).reshape(-1,1)
-    notas = np.asarray(data['Nota']).reshape(-1,1)
-    features = np.concatenate((notas , percFalta),axis=1)
+    p1 = np.asarray(data['P1']).reshape(-1,1)
+    p2 = np.asarray(data['P2']).reshape(-1,1)
+    features = np.concatenate((p1, p2, percFalta),axis=1)
     target = data['resultado']
 
 
     Arvore = DecisionTreeClassifier(criterion='entropy').fit(features, target)
 
     plt.figure(figsize=(10, 6.5))
-    tree.plot_tree(Arvore,feature_names=['Notas','PercFalta'],class_names=['Aprovado', 'Reprovado'],
+    tree.plot_tree(Arvore,feature_names=['P1','P2','PercFalta'],class_names=['Aprovado', 'Reprovado'],
                    filled=True, rounded=True)
     plt.show()
 
     fig, ax = plt.subplots(figsize=(25, 10))
     metrics.plot_confusion_matrix(Arvore,features,target,display_labels=['Aprovado', 'Reprovado'], values_format='d', ax=ax)
     plt.show()
+
+criterioMedia()
